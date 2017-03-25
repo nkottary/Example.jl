08f2be49222901af1b375de02ec61dcfaefad4bc
nishanthkottary <nishanth@juliacomputing.com>
2017-03-25 12:38:28 +0530

    fix benchmark script

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,6 +1,6 @@
 using PkgBenchmark
 
 @benchgroup "test_bench" begin
-    @bench "one_plus_one", 1+1
-    @bench "mult_test", rand(3, 2) * rand(2, 4)
+    @bench "one_plus_one" 1+1
+    @bench "mult_test" rand(3, 2) * rand(2, 4)
 end
